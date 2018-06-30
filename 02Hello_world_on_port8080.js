fab1f185cfb3b47acfaea03309abeef175168edb
Chao Ching Tou <air_jordan981182@yahoo.com.tw>
2018-06-30 16:19:50 +0800

    Hello world on port 8080

---
+++ 02Hello_world_on_port8080.js
@@ -0,0 +1,9 @@
+/**
+ * Created by air_j on 2018/6/30.
+ */
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
